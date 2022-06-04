d8a20f4c1ea0906480089ba121811319944e3db8
milesd <milesd@chocksaway.com>
2022-06-04 18:00:57 +0100

    Refactored.  Changing to MongoDB.

--- src/main/java/com/chocksaway/gallery/GalleryApplication.java
+++ src/main/java/com/chocksaway/gallery/GalleryApplication.java
@@ -2,16 +2,11 @@ package com.chocksaway.gallery;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-import io.r2dbc.spi.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
